9714440a9a18e1ea30d11d1f6ca9870a5cfc77c1
dragonworx <dragonworxau@yahoo.com.au>
2023-09-30 11:11:48 +1000

    add css class support

--- src/lib/button.ts
+++ src/lib/button.ts
@@ -1,6 +1,6 @@
 import { Component } from './component';
 
-export class Button extends Component<string, HTMLButtonElement> {
+export class Button extends Component<HTMLButtonElement, string> {
 	static componentId = 'button';
 
 	protected defaults(): string {
