b891ad00506447f4ce8ce467a48967e8b963de81
dragonworx <dragonworxau@yahoo.com.au>
2023-09-30 23:21:34 +1000

    fix example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,11 +10,13 @@
 	let checkbox = new Checkbox();
 	let list = new StringList(['test1', 'test2', 'test3']);
 
+	type MyEvent = 'foo' | 'bar' | 'hover';
+
 	onMount(() => {
 		button.on('click', () => (checkbox.value = !checkbox.value));
-		list.on<'foo' | 'bar'>('foo', () => {});
+		list.on<MyEvent>('foo', () => {});
 		list.on('click', (e) => console.log(Component.owner(e)));
-		list.on<'hover'>('hover', (e) => console.log(e.detail));
+		list.on<MyEvent>('hover', (e) => console.log(e.detail));
 
 		button.appendTo(main);
 		checkbox.appendTo(main);
