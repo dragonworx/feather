57f013a7b6e5ad6306a4bf382508e50380e2ea6b
Ali Chamas <dragonworxau@yahoo.com.au>
2023-11-02 10:24:27 +1100

    wip

--- src/lib/control.ts
+++ src/lib/control.ts
@@ -1,4 +1,24 @@
-import type { ControlEventHandler, WithDescriptor, WithProps } from '.';
+export interface Descriptor<PropsType extends object = object>
+{
+    tagName?: string;
+    props: PropsType;
+    classes?: string[];
+    template?: HTMLElement | string;
+    watchAttributes?: string[];
+}
+
+export interface CustomEventListener
+{
+    (evt: CustomEvent): void;
+}
+
+export type ControlEventHandler = EventListener | CustomEventListener;
+
+export type Constructor<T> = new (...args: unknown[]) => T;
+
+export type WithDescriptor = { __descriptor: Descriptor };
+export type WithProps = { initialProps: object };
+export type WithAttributes = { observedAttributes: string[] };
 
 /** Base Control extends HTMLElement as Custom Element */
 export abstract class Control<
