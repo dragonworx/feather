84a09ec3a68f7c0c231e45258e04160818302476
Ali Chamas <dragonworxau@yahoo.com.au>
2023-11-01 17:51:36 +1100

    clean up

---
+++ src/lib/builder.ts
@@ -0,0 +1,39 @@
+import type { Constructor, Descriptor, WithAttributes, WithDescriptor, WithProps } from '.';
+import type { Control } from './control';
+import { toHyphenCase } from './util';
+
+export const tagPref = 'ctrl-';
+
+/** Custom Element registration function */
+export function Ctrl<P extends object, C extends Constructor<Control<P>>>(
+    htmlElementCtor: C,
+    descriptor: Descriptor<P>
+)
+{
+    const { tagName, watchAttributes } = descriptor;
+    const fullTagName = tagPref + (tagName ?? toHyphenCase(htmlElementCtor.name));
+
+    if (fullTagName.endsWith('-') || fullTagName.startsWith('-'))
+    {
+        throw new Error(`Invalid tag name: ${fullTagName}`)
+    }
+
+    /** Initialise Custom Class */
+    (htmlElementCtor as unknown as WithDescriptor).__descriptor = descriptor;
+    watchAttributes && ((htmlElementCtor as unknown as WithAttributes).observedAttributes = watchAttributes);
+
+    customElements.define(fullTagName, htmlElementCtor);
+
+    /** Custom Class Instantiating */
+    return class
+    {
+        constructor(props: Partial<P> = {})
+        {
+            const element = document.createElement(fullTagName) as InstanceType<C>;
+
+            (element as unknown as WithProps).initialProps = props;
+
+            return element;
+        }
+    } as unknown as new (props: Partial<P>) => InstanceType<C>;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,33 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { test } from '$lib/new/test2';
 	import './forceHotReload';
 
+	import { onMount } from 'svelte';
+	import { Button, type ButtonEvents } from '$lib/test';
+
 	onMount(() => {
-		test();
+		const root = document.getElementById('root') as HTMLElement;
+
+		/** Example Instantiation */
+		const button = new Button({ x: 5 });
+
+		button.textContent = 'Click me!';
+		button.test();
+		button.on('event1', () => console.log('event1'));
+		button.style.color = 'white';
+		button.onclick = () => button.remove();
+
+		setTimeout(() => {
+			button.emit<ButtonEvents['event1']>('event1', { foo: '123' });
+			button.setAttribute('size', '123');
+			button.removeAttribute('size');
+		}, 1000);
+
+		(window as any).button = button;
+
+		root.appendChild(button);
 	});
 </script>
 
 <main id="root">
 	<ctrl-button size="abc">foo</ctrl-button>
 </main>
-
-<style>
-	main {
-		width: 100%;
-		height: 100%;
-	}
-</style>
