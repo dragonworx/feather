54809c8c0ccafe8c4f6cd268eaa937fd985ba585
Ali Chamas <dragonworxau@yahoo.com.au>
2023-11-15 20:33:21 +1100

    wip

--- src/lib/components/accordion.svelte
+++ src/lib/components/accordion.svelte
@@ -11,7 +11,7 @@
 	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<legend on:click={toggle}>{title}</legend>
-	<section class:open={isOpen} class:closed={!isOpen}>
+	<section class="content" class:open={isOpen} class:closed={!isOpen}>
 		<slot />
 	</section>
 </fieldset>
